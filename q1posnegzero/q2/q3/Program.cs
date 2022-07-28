a34294c8fda99b43193b30435e2f75a58a0ac473
AravinthManikandan <manidon55677@gmail.com>
2022-07-28 19:39:17 +0530

    q3 completed

---
+++ q1posnegzero/q2/q3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace q3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a =Getfloat("enter the first  number");
+            PrintMessage(a.ToString());
+            float b =Getfloat("enter the second number");
+            PrintMessage(b.ToString());
+            if(a==b)
+            {
+                System.Console.WriteLine("floating are equal");
+            }
+            else
+            {
+                System.Console.WriteLine("floating are not equal");
+            }
+            
+        }
+    public static float Getfloat(string a)
+            {
+                PrintMessage(a);
+                float temp = float.Parse(Console.ReadLine());
+                return temp;
+
+            }
+    public static void PrintMessage(string Message)
+            {
+                System.Console.WriteLine(Message);
+            }
+    }
+}
