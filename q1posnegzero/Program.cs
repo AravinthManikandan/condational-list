7f8ba600e33976be02494613df70433d81821dbb
AravinthManikandan <manidon55677@gmail.com>
2022-07-28 16:08:42 +0530

    q1completed

---
+++ q1posnegzero/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace q1posnegzero
+{
+   
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int a =Getinteger("enter a number");
+            PrintMessage(a.ToString());
+            if (a>0)
+            {
+                System.Console.WriteLine( "positive");  
+            } 
+            else if(a==0)
+            {
+                System.Console.WriteLine("zero");
+            }
+            else
+            {
+                System.Console.WriteLine("negative");
+            }          
+           
+        }
+            
+        public static int Getinteger(string a)
+            {
+                PrintMessage(a);
+                int temp = int.Parse(Console.ReadLine());
+                return temp;
+
+            }
+        public static void PrintMessage(string Message)
+            {
+                System.Console.WriteLine(Message);
+            }
+      
+            
+        
+        
+    }
+}
