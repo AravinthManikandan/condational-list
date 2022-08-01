71aea8f251207b7a8d5492004c095779db703c33
AravinthManikandan <manidon55677@gmail.com>
2022-08-01 15:31:05 +0530

    q4 is completed

---
+++ q1posnegzero/q2/q3/q4/q4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace q4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           System.Console.WriteLine("enter a number");
+           int a = int.Parse(Console.ReadLine());
+
+           string num = Getnumber(a);
+           System.Console.WriteLine("number is "+num);
+           
+       
+           
+        }
+       
+    public static string Getnumber(int a)
+            {  string number=" ";
+                
+                switch (a%2)
+                {
+                  
+                    case 0:
+                           number = "even";
+                           break ;
+                    
+                    case 1:
+                           number = "odd";
+                           break ;
+                
+                
+                }
+                return number;
+          
+        
+            }
+    }
+}
